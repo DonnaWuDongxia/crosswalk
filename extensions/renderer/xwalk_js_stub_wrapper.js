bc54e0defc4bee880d2899df5a34cd7eaf0a8a13
Donna Wu <donna.wu@intel.com>
2015-06-04 15:49:20 +0800

    [Android] Add a js module for extension js-stub auto-generation
    This module is called "jsStub" extension js-stub can use this
    heper module by "requireNative("jsStub").jsStub;".
    More usage information please see the comments in the file.

---
+++ extensions/renderer/xwalk_js_stub_wrapper.js
@@ -0,0 +1,299 @@
+//This will be a native module, extension js stub can requireNative("jsStub") to get all the interfaces
+
+/*
+ * This is a internal js helper module in the extension's js context.
+ * Currently, this helper just be used by "auto-generated" js-stubs,
+ * following is an example js-stub using the heper.
+ * Usage example:
+ *     var jsStub = requireNative("jsStub").jsStub;
+ *     var helper = jsStub.create(exports, extension, false);
+ *
+ *     jsStub.makeEventTarget(exports);
+ *     helper.addEvent("updatePrefix");
+ *     helper.addEvent("click");
+ *
+ *     jsStub.defineProperty(exports, "prefix", true);
+ *
+ *     exports.echo = function(msg) {
+ *       return helper.invokeNative("echo", [msg], true);
+ *     }
+ *
+ *     exports.getPrefix = function(callback) {
+ *        helper.invokeNative("getPrefix", [callback]);
+ *     }
+ *
+ *     exports.getPrefixPromise = function() {
+ *        return new Promise(function(resolve, reject) {
+ *            helper.invokeNative('getPrefixPromise', [{'resolve': resolve, 'reject': reject}]);
+ *        });
+ *     }
+ *
+ */
+var jsStub = function(base, channel) {
+  var nextCallbackId = 1;
+
+  //refer to the exposed extension object
+  this.base = base;
+
+  //refer to the global extension variable, used to send message
+  this.channel = channel;
+  //retain the properties which is exposed by native
+  this.properties = {};
+  this.callbacks = [];
+
+  this.getCallbackId = function() {
+    while (this.callbacks[nextCallbackId] != undefined)
+      ++nextCallbackId;
+    return nextCallbackId;
+  };
+};
+
+jsStub.create = function(base, channel) {
+  var helper = jsStub.getHelper(base, channel);
+  channel.setMessageListener(function (msg) {
+    helper.handleMessage(msg);
+  });
+  return helper;
+};
+
+jsStub.getHelper = function(base, channel) {
+  if (!(base.__stubHelper instanceof jsStub)) {
+    base.__stubHelper = new jsStub(base, channel);
+  }
+  return base.__stubHelper;
+};
+
+function isSerializable(obj) {
+  if (!(obj instanceof Object))
+    return true;
+  if (obj instanceof Function)
+    return false;
+  if (obj instanceof Boolean ||
+      obj instanceof Date ||
+      obj instanceof Number ||
+      obj instanceof RegExp ||
+      obj instanceof String)
+    return true;
+  for (var p of Object.getOwnPropertyNames(obj))
+    if (!isSerializable(obj[p]))
+      return false;
+  return true;
+}
+
+function objectRef(cid, vid) {
+  return (cid << 8) + vid;
+}
+
+jsStub.prototype = {
+  "invokeNative": function(name, args, sync) {
+    if (!Array.isArray(args)) {
+      console.warn("invokeNative: args is not an array.");
+      args = Array(args);
+    }
+
+    // Retain callbacks in JS stub, replace them to related number ID
+    var call = [];
+    var cid = this.getCallbackId();
+    args.forEach(function(val, vid, a) {
+      if (!isSerializable(val)) {
+        call[vid] = val;
+        a[vid] = objectRef(cid, vid);
+      }
+    })
+
+    if (call.length > 0) {
+      this.callbacks[cid] = call;
+    }
+
+    var msg = {
+      cmd: "invokeNative",
+      name: name,
+      args: args
+    };
+    if (sync)
+      return this.sendSyncMessage(msg);
+    else
+      this.postMessage(msg);
+  },
+  "getNativeProperty": function(name) {
+    return this.sendSyncMessage({
+      cmd: "getProperty",
+      name: name
+    });
+  },
+  "setNativeProperty": function(name, value) {
+    this.sendSyncMessage({
+      cmd: "setProperty",
+      name: name,
+      value: value
+    });
+  },
+  },
+  "invokeCallback": function(id, key, args) {
+      var cid = id >>> 8;
+      var vid = id & 0xFF;
+      var obj = this.callbacks[cid][vid];
+      if (typeof(key) === 'number' || key instanceof Number)
+          obj = obj[key];
+      else if (typeof(key) === 'string' || key instanceof String)
+          key.split('.').forEach(function(p){ obj = obj[p]; });
+
+      if (obj instanceof Function)
+          obj.apply(null, JSON.parse(args));
+  },
+  "handleMessage": function(json) {
+    var msg = JSON.parse(json);
+    if (!msg.cmd)
+      console.warn("No valid Java CMD.");
+    switch (msg.cmd) {
+      case "invokeCallback":
+        this.invokeCallback(msg.cid, msg.key, msg.args);
+        break;
+      case "updateProperty":
+        // Case: property is changed by native and need
+        // to sync its value to JS side.
+        if(this.properties.hasOwnProperty(msg.name) >= 0) {
+          this[msg.name] = this.getNativeProperty(msg.name);
+        }
+        break;
+      case "error":
+        // supported key: log, info, warn, error
+        console[msg.level](msg.msg);
+        break;
+      case "dispatchEvent":
+        if(msg.type) {
+          this.base.dispatchEvent(msg.type, msg.event);
+        }
+        break;
+      default:
+        console.warn("Unsupported Java CMD:" + msg.cmd);
+    }
+  },
+  sendSyncMessage: function(msg) {
+    var resultStr = this.channel.internal.sendSyncMessage(JSON.stringify(msg));
+    return resultStr.length > 0 ? JSON.parse(resultStr) : undefined;
+  },
+
+  postMessage: function(msg) {
+    this.channel.postMessage(JSON.stringify(msg));
+  },
+
+  "destory": function() {}
+};
+
+// expose native property to JavaScript
+jsStub.defineProperty = function(obj, prop, writable) {
+  var helper = jsStub.getHelper(obj);
+  var value = helper.getNativeProperty(prop);
+
+  // keep all exported properties in the helper
+  helper.properties[prop] = helper.getNativeProperty(prop);
+
+  var desc = {
+    'configurable': false,
+    'enumerable': true,
+    'get': function() { return helper.properties[prop]; }
+  }
+  if (writable) {
+    desc.set = function(v) {
+      helper.setNativeProperty(prop, v);
+      helper.properties[prop] = v;
+    }
+  }
+  Object.defineProperty(obj, prop, desc);
+};
+
+// EventTarget implementation:
+// When dispatch event in the Java side, we will broadcast the
+// message to all instances, so all frames will handle the event.
+jsStub.makeEventTarget = function(base) {
+  var helper = jsStub.getHelper(base);
+  helper.event_listeners = {};
+
+  helper.addEvent = function(type) {
+    Object.defineProperty(helper, "_on" + type, {
+      writable : true,
+    });
+
+    Object.defineProperty(base, "on" + type, {
+      get: function() {
+        return helper["_on" + type];
+      },
+      set: function(listener) {
+        var old_listener = helper["_on" + type];
+        if (old_listener === listener)
+          return;
+
+        if (old_listener)
+          base.removeEventListener(type, old_listener);
+
+        helper["_on" + type] = listener;
+        base.addEventListener(type, listener);
+      },
+      enumerable: true,
+    });
+
+  };
+
+  function dispatchEvent(type, event) {
+    if (!(type instanceof String) || !(("on" + type) in base))
+      console.warn("Invalid or unsupported event type:" + type);
+      return;
+    if (!(type in helper.event_listeners))
+      return;
+
+    var listeners = helper.event_listeners[type];
+    for (var i in listeners)
+      listeners[i](event);
+  };
+
+  function addEventListener(type, listener) {
+    if (!(listener instanceof Function))
+      console.warn("Illegal argument, expect a function.");
+      return;
+
+    if (!(type instanceof String) || !(("on" + type) in base))
+      console.warn("Invalid or unsupported event type:" + type);
+      return;
+
+    if (type in helper.event_listeners) {
+      var listeners = helper.event_listeners[type];
+      if (listeners.indexOf(listener) == -1)
+        listeners.push(listener);
+    } else {
+      helper.event_listeners[type] = [listener];
+    }
+  };
+
+  function removeEventListener(type, listener) {
+    if (!(listener instanceof Function))
+      console.warn("Illegal argument, expect a function.");
+      return;
+
+    if (!(type in helper.event_listeners))
+      console.warn("Invalid or unsupported event type:" + type);
+      return;
+
+    var listeners = helper.event_listeners[type];
+    var index = listeners.indexOf(listener);
+    if(index > -1)
+      listeners.splice(index, 1);
+  };
+
+  Object.defineProperties(base, {
+    "addEventListener" : {
+      value : addEventListener,
+      enumerable : true,
+    },
+    "removeEventListener" : {
+      value : removeEventListener,
+      enumerable : true,
+    },
+    "dispatchEvent" : {
+      value : dispatchEvent,
+      enumerable : true,
+    },
+  });
+};
+
+exports.jsStub = jsStub;
